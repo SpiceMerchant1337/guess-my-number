a93e3d0707eb767fc6498473afe866e559eccd96
SpiceMerchant1337 <88102936+SpiceMerchant1337@users.noreply.github.com>
2023-01-02 11:27:31 +0300

    Finish lect 77: implement high score

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ document.querySelector(".score").textContent = 17; */
 
 let rngNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
@@ -26,6 +27,11 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = rngNumber;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(".highscore").textContent = highScore;
+    }
 
     // When player guess is too high
   } else if (guess > rngNumber) {
